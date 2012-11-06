168a12548ba7a9467c619018980663e326e2ec34
mioki <mioki@hummingbird.ad.jp.omron.net>
2012-11-06 19:00:27 +0900

    ThoughtWorksアンソロジー ルール2まで完了しました。

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -10,7 +10,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 10, 20, 30, 40, 50, 2 };
 		
-		coinCounter.setCoin( coin );
+		coinCounter.setCoins( coin );
 		coinCounter.setPrice( 500 );
 		assertEquals( coinCounter.toString(), new String("1(500â~ã 1ñáÇÃçáåv1ñá)") );
 	}
@@ -21,7 +21,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 1000, 1, 1, 1, 1, 1 };
 		
-		coinCounter.setCoin(coin);
+		coinCounter.setCoins(coin);
 		coinCounter.setPrice(700);
 		System.out.println(coinCounter.toString());
 		assertEquals( coinCounter.toString(), new String("40(500â~ã 1ñáÅA100â~ã 1ñáÅA50â~ã 1ñáÅA10â~ã 1ñáÅA5â~ã 1ñáÅA1â~ã 35ñáÇÃçáåv40ñá)") );
@@ -33,7 +33,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 10, 20, 30, 40, 50, 2 };
 		
-		coinCounter.setCoin(coin);
+		coinCounter.setCoins(coin);
 		coinCounter.setPrice(0);
 		assertEquals( coinCounter.toString(), new String("0(çáåv0ñá)") );
 	}
@@ -44,7 +44,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000 };
 		
-		coinCounter.setCoin(coin);
+		coinCounter.setCoins(coin);
 		coinCounter.setPrice(1000);
 		assertEquals( coinCounter.toString(), new String("2(500â~ã 2ñáÇÃçáåv2ñá)") );
 	}
@@ -55,7 +55,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 1000000001, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000 };
 		
-		coinCounter.setCoin(coin);
+		coinCounter.setCoins(coin);
 		coinCounter.setPrice(1000);
 		assertEquals( coinCounter.toString(), new String("çdâ›ÇÃñáêîÇ™ïsê≥") );
 	}
@@ -66,7 +66,7 @@ public class FirstTest {
 		CoinCounter coinCounter = new CoinCounter();
 		int coin[] = { 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000 };
 		
-		coinCounter.setCoin(coin);
+		coinCounter.setCoins(coin);
 		coinCounter.setPrice(1000000000);
 		assertEquals( coinCounter.toString(), new String("2000000(500â~ã 2000000ñáÇÃçáåv2000000ñá)") );
 	}
